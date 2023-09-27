eb4b36da9715fd3b3c0c180d75c77109a42caa38
Aarthi-aaru <145124809+Aarthi-aaru@users.noreply.github.com>
2023-09-27 08:42:26 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-year = 2023
-if (year % 4 == 0):
-  print("leap Year")
-else:
-  print("not Leap Year")
+#
+def linear_search_product(product_list, target_product):
+    indices = []
+    for i, product in enumerate(product_list):
+        if product == target_product:
+            indices.append(i)
+    return indices
+
+products = ["Apple", "Banana", "Apple", "Orange", "Pear", "Apple"]
+target = "Apple"
+result = linear_search_product(products, target)
+print(result)
